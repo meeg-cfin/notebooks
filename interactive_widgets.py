d8affe58cffb69180d5fee31afa21d51804952b3
Christopher J. Bailey <bailey.cj@gmail.com>
2015-09-13 13:40:36 +0200

    made a class out of it and succeeded

--- interactive_widgets.py
+++ interactive_widgets.py
@@ -2,57 +2,63 @@ from IPython.html import widgets
 from IPython.html.widgets import interact
 from IPython.display import display
 
-subj_dd  = widgets.Dropdown(options=['']+db.get_subjects(), description='Subject ID')
-modality = widgets.RadioButtons(options=['MEG','MR'], description='Modality', visible=True)
-study_dd = widgets.Dropdown(options=['---'], description='Study date', visible=True)
-series_dd = widgets.Dropdown(options=dict([['---','0']]), description='Series name', visible=True)
-files_list = widgets.Textarea(description="Files", disabled=True, visible=True)
+class StormdbAccess():
 
-info = dict(subj_id='', modality='MEG', study='---', series=0)
+    def __init__(self, db=None):
+        self.db = db
+        self.info = dict(subj_id='', modality='MEG', study='---', series=0)
 
-def get_studies(subj_id, modality):
-    if len(subj_id) > 0:
-        # These need to be up-front, since study_dd and series_dd .options
-        # get set below, WHICH TRIGGERS THEIR CALLBACKS!!
-        info['subj_id'] = subj_id
-        info['modality'] = modality
+        self.subj_dd  = widgets.Dropdown(options=['']+self.db.get_subjects(), description='Subject ID')
+        self.modality = widgets.RadioButtons(options=['MEG','MR'], description='Modality', visible=True)
+        self.study_dd = widgets.Dropdown(options=['---'], description='Study date', visible=True)
+        self.series_dd = widgets.Dropdown(options=dict([['---','0']]), description='Series name', visible=True)
+        self.files_list = widgets.Textarea(description="Files", disabled=True, visible=True)
 
-        studs = db.get_studies(subj_id, modality=modality, unique=False)
-        if len(studs) > 0:
-            if type(studs) is str:
-                studs = [studs,]
-            study_dd.options = ['---'] + studs
-            series_dd.options = dict([['---','0']])
+        self.subj_i  = widgets.interactive(self.get_studies, subj_id=self.subj_dd, modality=self.modality)
+        self.study_i = widgets.interactive(self.get_series, study=self.study_dd)
+        self.series_i = widgets.interactive(self.get_files, series=self.series_dd)
+
+
+    def get_studies(self, subj_id, modality):
+        if len(subj_id) > 0:
+            # These need to be up-front, since study_dd and series_dd .options
+            # get set below, WHICH TRIGGERS THEIR CALLBACKS!!
+            self.info['subj_id'] = subj_id
+            self.info['modality'] = modality
+
+            studs = self.db.get_studies(subj_id, modality=modality, unique=False)
+            if len(studs) > 0:
+                if type(studs) is str:
+                    studs = [studs,]
+                self.study_dd.options = ['---'] + studs
+                self.series_dd.options = dict([['---','0']])
+            else:
+                self.study_dd.options = ['---',]
+
+            self.files_list.value = ''
+
+    def get_series(self, study):
+        if not study == '---':
+            series_dict = self.db.get_series(self.info['subj_id'], study, self.info['modality'])
+            series_dict.update({'---': '0'})
+            self.series_dd.options = sorted(series_dict.items(), key=lambda t: t[1])
+            self.info['study'] = study
         else:
-            study_dd.options = ['---',]
+            self.series_dd.options = {'---': '0'}
+            self.files_list.value = ''
 
-        files_list.value = ''
-            
-def get_series(study):
-    if not study == '---':
-        series_dict = db.get_series(info['subj_id'], study, info['modality'])
-        series_dict.update({'---': '0'})
-        series_dd.options = sorted(series_dict.items(), key=lambda t: t[1])
-        info['study'] = study
-    else:
-        series_dd.options = {'---': '0'}
-        files_list.value = ''
+    def get_files(self, series):
+        if int(series) > 0:
+            rawfiles = self.db.get_files(self.info['subj_id'], self.info['study'], 
+                                    self.info['modality'], series)
+            self.files_list.value = "\n".join(rawfiles) if type(rawfiles) is list else ''
+            self.info['series'] = int(series)
+        else:
+            self.files_list.value = ''
 
-def get_files(series):
-    if int(series) > 0:
-        rawfiles = db.get_files(info['subj_id'], info['study'], 
-                                info['modality'], series)
-        files_list.value = "\n".join(rawfiles) if type(rawfiles) is list else ''
-        info['series'] = int(series)
-    else:
-        files_list.value = ''
 
-subj_i  = widgets.interactive(get_studies, subj_id=subj_dd, modality=modality)
-study_i = widgets.interactive(get_series, study=study_dd)
-series_i = widgets.interactive(get_files, series=series_dd)
-
-def display_StormDB_access():
-    display(subj_i)
-    display(study_i)
-    display(series_i)
-    display(files_list)
+    def display_StormDB_access(self):
+        display(self.subj_i)
+        display(self.study_i)
+        display(self.series_i)
+        display(self.files_list)
