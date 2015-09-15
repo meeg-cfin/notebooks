bcd2bbde59dbd3b10ada31f1b65349d7582fb4d1
Christopher J. Bailey <bailey.cj@gmail.com>
2015-09-15 09:30:58 +0200

    database query example working

--- interactive_widgets.py
+++ interactive_widgets.py
@@ -2,31 +2,40 @@ from IPython.html import widgets
 from IPython.html.widgets import interact
 from IPython.display import display
 
-class StormdbAccess():
+class FakeDatabaseQuery():
+    def __init__(self):
+        raise NotImplementedError('Would be usefull for debugging, though...')
 
-    def __init__(self, db=None):
-        self.db = db
-        self.info = dict(subj_id='', modality='MEG', study='---', series=0)
+class InteractiveQuery():
 
-        self.subj_dd  = widgets.Dropdown(options=['']+self.db.get_subjects(), description='Subject ID')
+    def __init__(self, query=None):
+        if query is None:
+            self.query = FakeDatabaseQuery()
+        else:
+            self.query = query
+            
+        self.info = dict(subj_id=None, modality=None, study=None, 
+                         series=None, series_name=None, files=None)
+
+        self.subj_dd  = widgets.Dropdown(options=['']+self.query.get_subjects(), description='Subject ID')
         self.modality = widgets.RadioButtons(options=['MEG','MR'], description='Modality', visible=True)
         self.study_dd = widgets.Dropdown(options=['---'], description='Study date', visible=True)
         self.series_dd = widgets.Dropdown(options=dict([['---','0']]), description='Series name', visible=True)
         self.files_list = widgets.Textarea(description="Files", disabled=True, visible=True)
 
-        self.subj_i  = widgets.interactive(self.get_studies, subj_id=self.subj_dd, modality=self.modality)
-        self.study_i = widgets.interactive(self.get_series, study=self.study_dd)
-        self.series_i = widgets.interactive(self.get_files, series=self.series_dd)
+        self.subj_i  = widgets.interactive(self._get_studies, subj_id=self.subj_dd, modality=self.modality)
+        self.study_i = widgets.interactive(self._get_series, study=self.study_dd)
+        self.series_i = widgets.interactive(self._get_files, series=self.series_dd)
 
 
-    def get_studies(self, subj_id, modality):
+    def _get_studies(self, subj_id, modality):
         if len(subj_id) > 0:
             # These need to be up-front, since study_dd and series_dd .options
             # get set below, WHICH TRIGGERS THEIR CALLBACKS!!
             self.info['subj_id'] = subj_id
             self.info['modality'] = modality
 
-            studs = self.db.get_studies(subj_id, modality=modality, unique=False)
+            studs = self.query.get_studies(subj_id, modality=modality, unique=False)
             if len(studs) > 0:
                 if type(studs) is str:
                     studs = [studs,]
@@ -37,9 +46,9 @@ class StormdbAccess():
 
             self.files_list.value = ''
 
-    def get_series(self, study):
+    def _get_series(self, study):
         if not study == '---':
-            series_dict = self.db.get_series(self.info['subj_id'], study, self.info['modality'])
+            series_dict = self.query.get_series(self.info['subj_id'], study, self.info['modality'])
             series_dict.update({'---': '0'})
             self.series_dd.options = sorted(series_dict.items(), key=lambda t: t[1])
             self.info['study'] = study
@@ -47,18 +56,24 @@ class StormdbAccess():
             self.series_dd.options = {'---': '0'}
             self.files_list.value = ''
 
-    def get_files(self, series):
+    def _get_files(self, series):
         if int(series) > 0:
-            rawfiles = self.db.get_files(self.info['subj_id'], self.info['study'], 
+            rawfiles = self.query.get_files(self.info['subj_id'], self.info['study'], 
                                     self.info['modality'], series)
             self.files_list.value = "\n".join(rawfiles) if type(rawfiles) is list else ''
             self.info['series'] = int(series)
+            self.info['files'] = rawfiles
+            # Some extra spice here: get the series name too
+            inverted_mapping = dict((v,k) for k, v in dict(self.series_dd.options).items())
+            self.info['series_name'] = inverted_mapping[series]
         else:
             self.files_list.value = ''
 
 
-    def display_StormDB_access(self):
+    def display(self):
         display(self.subj_i)
         display(self.study_i)
         display(self.series_i)
         display(self.files_list)
+        
+        
